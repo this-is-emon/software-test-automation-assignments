c62d7ad54ad7748f41fb6f887ece082221b9c8d9
this-is-emon <mofijul.islam.emon@gmail.com>
2025-10-20 18:35:00 -0400

    Assignment-1:'writing 1stcode' uploaded

---
+++ Module_1/assignment1JavaSyntax/MySelf.java
@@ -0,0 +1,42 @@
+/*
+Assignment_1_Writing_firstCode_Java:
+Step 1: Create a Project name "AssignmentYourName", For example, AssignmentAlex.
+
+Step 2: Create a package name "assignment1JavaSyntax" inside the src file of the project.
+
+Step 3: Create a class name "MySelf" (Please don't select the main method while creating a Class). 
+		Can you get the main method by a shortcut? If yes, please do so.
+
+Step 4: Print Your name and address in different lines inside the main method. 
+		You can use the knowledge how to copy a line to do so [try to follow how to copy and paste a line in IDE by keyboard command]. 
+		Can you see the print outcome in the console?
+
+Step 5: Now, Use multiple line comments between package and Class.
+ 		Write there the naming convention of Project, package, and Class. (Please get the help of class notes).
+
+Step 6: Use also single-line comments just before the main method and 
+		write about what you know about the main method (please get the help of class notes).
+
+Step 7: Organize the code [Put your cursor in the Editor, and type shift + ctrl/Command + F to organize the code].
+
+Step 8: Now Copy the code and paste it below as your answer.
+*/
+
+package assignment1JavaSyntax; // Step 2: Package name: "assignment1JavaSyntax" 
+
+/*
+ Step 5: Multiple line comments between package and Class.
+*/
+
+// Step 3: Class name : "MySelf"
+public class MySelf {
+
+	// Step 3: Main method, Step 6: This is the entry point of a JAVA program
+	public static void main(String[] args) {
+
+		// Step 4: Printing my "name" and "address"
+		System.out.println("Emon");
+		System.out.println("Jamaica,NYC");
+	}
+
+}
